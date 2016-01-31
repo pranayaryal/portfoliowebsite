2646293177321d7f70e8176287c8c4fb957ec778
pranayaryal <drpranayaryal@gmail.com>
2016-01-31 03:15:45 -0500

    m

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -39,7 +39,7 @@
 <div class="container">
     <div class="content">
         <div class="title">
-            Weelcome to Pranay Ayl nice
+            Weeelcome to Pranay Ayl nice
 
 
         </div>
