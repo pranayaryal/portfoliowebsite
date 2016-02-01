eb4d3707e4e43657ca55575b8f2019cbd9143966
pranayaryal <drpranayaryal@gmail.com>
2016-01-31 22:42:19 -0500

    modified helo

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -39,7 +39,7 @@
 <div class="container">
     <div class="content">
         <div class="title">
-            Hello how are you all doing. I am doing fine 
+            Hello how are you all doing. I am doing find. And I love you
 
         </div>
 
