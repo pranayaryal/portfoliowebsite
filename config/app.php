fb8dcee8611078330606fe0aadbb14684d98e637
pranayaryal <drpranayaryal@gmail.com>
2016-02-11 11:14:44 -0500

    added illuminate/html

--- config/app.php
+++ config/app.php
@@ -136,7 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+//        Illuminate\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
--- public/index.php
+++ public/index.php
@@ -19,9 +19,9 @@
 |
 */
 
-//require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
-$compiledPath = __DIR__.'/../vendor/compiled.php';
+//$compiledPath = __DIR__.'/../vendor/compiled.php';
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
