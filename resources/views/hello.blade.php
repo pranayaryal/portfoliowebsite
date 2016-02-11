b8879e0115aaf8ea4451d73311a2386e35d2bc35
pranayaryal <drpranayaryal@gmail.com>
2016-02-11 13:21:06 -0500

    added hello

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -39,7 +39,7 @@
 <div class="container">
     <div class="content">
         <div class="title">
-		 Hello this is pranayaryal and I boring
+		 Hello this is pranayaryal and I boriiiiiiiiiiiiinga
         </div>
 
     </div>
