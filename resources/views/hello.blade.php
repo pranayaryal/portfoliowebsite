a0775856e2438f0200e14e81fb8d48177ddd3063
pranayaryal <drpranayaryal@gmail.com>
2016-02-11 09:44:50 -0500

    changed hello.blade

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -39,7 +39,7 @@
 <div class="container">
     <div class="content">
         <div class="title">
-		Hello heousdas;lkdjflasdjlaksdfasldfasdflasdlfkalsdfjlasfdlkasdflkasdlkffow are you?
+		 Hello
         </div>
 
     </div>
