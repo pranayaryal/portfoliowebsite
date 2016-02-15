f783bdb5f1bd810dd36cae9d53ab28a721b746d7
pranayaryal <drpranayaryal@gmail.com>
2016-02-15 01:06:38 -0500

    added the blog link

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -6,6 +6,7 @@
         <li class="nav__item"><a href="/about">About</a></li>
         <li class="nav__item"><a href="/contact">Contact</a></li>
         <li class="nav__item--current"><a href="/blog">Blog</a></li>
+        <li class="nav__item"><a href="/payments">Payments</a></li>
 
     </ul>
 @stop
