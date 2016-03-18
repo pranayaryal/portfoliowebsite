a470bb3b435b8547377d0fbad4f7ec1a07a68c65
pranayaryal <drpranayaryal@gmail.com>
2016-03-18 13:36:57 -0400

    changed back to blog

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
         <li class="nav__item"><a href="/">Portfolio</a></li>
         <li class="nav__item--current"><a href="/about">About</a></li>
         <li class="nav__item"><a href="/contact">Contact</a></li>
-        <li class="nav__item"><a href="/blog">E-commerce</a></li>
+        <li class="nav__item"><a href="/blog">Blog</a></li>
         <li class="nav__item"><a href="/payments">Payments</a></li>
 
     </ul>
