e1164d3baaeeac33c9f5fa8a6be1bc31f8012975
pranayaryal <drpranayaryal@gmail.com>
2016-03-18 23:49:50 -0400

    add blog

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -14,7 +14,7 @@
 
 @section('content')
     <div class="grid">
-        <p>Average Hospital Bill Payments By State</p>
+        <p>Average Hospital Bill Payments By State(Data from data.gov)</p>
     </div>
     <script src="http://d3js.org/d3.v3.min.js"></script>
     <script src="http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"></script>
