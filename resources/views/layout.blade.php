80eb866923566b4044f97bfaa9728cf91dfd0ddf
pranayaryal <drpranayaryal@gmail.com>
2016-03-18 23:30:03 -0400

    add blog

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,11 @@
     <title>Pranay Aryal/Web Developer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/application.css">
+    <link rel="stylesheet" href="/css/app.css">
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <link rel="stylesheet" href="/css/libs.css">
+
 </head>
 <body>
 
