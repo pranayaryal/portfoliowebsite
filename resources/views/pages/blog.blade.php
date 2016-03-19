835af6f1208ebafbfa0919b54c2cec53e43a2622
pranayaryal <drpranayaryal@gmail.com>
2016-03-18 23:00:52 -0400

    add blog

---
+++ resources/views/pages/blog.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('nav')
+    <ul class="nav is-collapsed-mobile">
+        <li class="nav__item"><a href="/">Portfolio</a></li>
+        <li class="nav__item"><a href="/about">About</a></li>
+        <li class="nav__item"><a href="/contact">Contact</a></li>
+        <li class="nav__item--current"><a href="/ecommerce">E-commerce</a></li>
+        <li class="nav__item"><a href="/payments">Payments</a></li>
+        <li class="nav__item"><a href="/blog">Blog</a></li>
+
+    </ul>
+@stop
+
+@section('content')
+    <div class="grid">
+        <p>This is only for testing purposes</p>
+    </div>
+
+@stop
