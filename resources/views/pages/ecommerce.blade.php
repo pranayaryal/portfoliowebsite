1ab609ab967f31725a0ca9e5fed013a0a874ed62
pranayaryal <drpranayaryal@gmail.com>
2016-03-19 00:37:40 -0400

    add blog

--- resources/views/pages/ecommerce.blade.php
+++ resources/views/pages/ecommerce.blade.php
@@ -15,7 +15,7 @@
 @section('content')
     <div class="grid">
         <div class="grid__col--4">
-            <p>I created this drawing app and I want to sell it to you</p>
+            <p>I created this drawing app. Do you want to buy it?</p>
             <a href="http://pranayaryal.github.io/drawing" target="_blank">
                 <img class="img--wrap" src="img/pic.png" alt="Avatar">
             </a>
