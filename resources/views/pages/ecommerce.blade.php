e67949b2b8fc9dd118e03eee84c5ebda4cbb9b25
pranayaryal <drpranayaryal@gmail.com>
2016-03-20 01:44:39 -0400

    add blog

--- resources/views/pages/ecommerce.blade.php
+++ resources/views/pages/ecommerce.blade.php
@@ -20,7 +20,13 @@
                 <img class="img--wrap" src="img/pic.png" alt="Avatar">
             </a>
             <p>Price: $50</p>
-
+            <span>Qty</span>
+            <select>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="2">3</option>
+                <option value="4">4</option>
+            </select>
             <a class="btn--success" href="#" >Add to Cart</a>
             <a class="btn--info" href="#" >Wish List</a>
 
@@ -33,12 +39,6 @@
                 <img class="img--wrap" src="img/form.png" alt="Avatar">
             </a>
             <p>Price: $20</p>
-            <a class="btn--success" href="#" id="cartadd" data-value="3">Add to Cart</a>
-            <a class="btn--info" href="#" >Wish List</a>
-        </div>
-        <div class="grid__col--4" id="circles">
-            <br><br>
-            <p class="cartnumber">0</p>
             <span>Qty</span>
             <select>
                 <option value="1">1</option>
@@ -46,6 +46,13 @@
                 <option value="2">3</option>
                 <option value="4">4</option>
             </select>
+            <a class="btn--success" href="#" id="cartadd" data-value="3">Add to Cart</a>
+            <a class="btn--info" href="#" >Wish List</a>
+        </div>
+        <div class="grid__col--4" id="circles">
+            <br><br>
+            <p class="cartnumber">0</p>
+
             <p>Item in cart</p>
             <a class="btn--warning" href="#" id="checkout" data-value=[]>Checkout</a>
 
