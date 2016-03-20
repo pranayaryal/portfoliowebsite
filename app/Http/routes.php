9b7a0f4547cdf067c6b3ec4428dd0979aa345455
pranayaryal <drpranayaryal@gmail.com>
2016-03-20 18:32:34 -0400

    add blog

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -50,9 +50,9 @@ use App\Http\Flash;
         return view('pages.payments');
     });
 
-    Route::post('/cart/add', function ()
+    Route::post('/cart/add', function (Request $request)
     {
-        var_dump("Congratulations you clicked " . \Input::get('item'));
+        var_dump("Congratulations you clicked " . $request->get('item'));
 
     });
 
